aac6508595e559cdda5ac7c7c97f7c472c563854
Eric Draut <edraut@gmail.com>
2020-04-05 12:39:29 -0600

    bump ubuntu version for mysql binary to be compatible with heroku-18

--- lib/build_pack/downloader.rb
+++ lib/build_pack/downloader.rb
@@ -5,7 +5,7 @@ module BuildPack
     MYSQL_BASE_URL = "http://security.ubuntu.com/ubuntu/pool/main/m/mysql-5.7/"
 
     # example client: "mysql-client-5.7_5.7.22-0ubuntu0.16.04.1_amd64.deb"
-    REGEX = /.*(mysql-client-5\.7_5\.7\.\d\d-0ubuntu0\.16\.\d\d\.\d_amd64.deb).*/
+    REGEX = /.*(mysql-client-5\.7_5\.7\.\d\d-0ubuntu0\.18\.\d\d\.\d_amd64.deb).*/
 
     class << self
       def download_latest_client_to(path)
